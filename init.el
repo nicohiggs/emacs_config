bec11ac3808882c99f6867def44b78100f37d950
Nico Higgs <nicoreekohiggs@gmail.com>
2023-04-09 22:36:57 -0600

    updated init el

--- init.el
+++ init.el
@@ -11,32 +11,10 @@
 (tool-bar-mode 0)
 
 ;; colour theme
+(load-theme 'solarized-selenized-white t)
 (global-hl-line-mode 1)
-(set-face-background 'hl-line "pale turquoise")
 (add-to-list 'default-frame-alist '(font . "Cascadia Mono-11.5"))
 (set-face-attribute 'default t :font "Cascadia Mono-11.5")
-(set-face-attribute 'font-lock-builtin-face nil :foreground "blue1")
-(set-face-attribute 'font-lock-comment-face nil :foreground "#00a000")
-(set-face-attribute 'font-lock-constant-face nil :foreground "red")
-(set-face-attribute 'font-lock-doc-face nil :foreground "#69B469")
-(set-face-attribute 'font-lock-function-name-face nil :foreground "gray25")
-(set-face-attribute 'font-lock-keyword-face nil :foreground "blue1")
-(set-face-attribute 'font-lock-string-face nil :foreground "purple")
-(set-face-attribute 'font-lock-type-face nil :foreground "blue1")
-(set-face-attribute 'font-lock-variable-name-face nil :foreground "gray25")
-
-(set-foreground-color "gray25")
-(set-background-color "gray97")
-(set-cursor-color "chocolate1")
-
-;; brackets
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(show-paren-match ((t (:background "#40ff40"))))
- '(show-paren-mismatch ((((class color)) (:background "red" :foreground "white")))))
 
 ;; solid cursor
 (blink-cursor-mode 0)
@@ -51,4 +29,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit)))
+ '(custom-safe-themes
+   '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "f5b6be56c9de9fd8bdd42e0c05fecb002dedb8f48a5f00e769370e4517dde0e8" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "efcecf09905ff85a7c80025551c657299a4d18c5fcfedd3b2f2b6287e4edd659" "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
+ '(package-selected-packages '(solarized-theme magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(show-paren-match ((t (:background "#40ff40"))))
+ '(show-paren-mismatch ((((class color)) (:background "red" :foreground "white")))))
+
+
