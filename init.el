ee124f033e7622c3bad63f5a12cb887174acf9b1
Nico Higgs <nicoreekohiggs@gmail.com>
2023-05-01 23:20:41 -0600

    changed theme and tried to add helm

--- init.el
+++ init.el
@@ -3,25 +3,25 @@
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 
-;; don't show the start screen
-(setq inhibit-startup-message t)
-
 ;; removing menu bar and tool bar
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 
 ;; colour theme
-(load-theme 'solarized-light t)
+(load-theme 'leuven t)
 (global-hl-line-mode 1)
 (add-to-list 'default-frame-alist '(font . "Cascadia Mono-11.5"))
 (set-face-attribute 'default t :font "Cascadia Mono-11.5")
 
-;; solid cursor
-;; (blink-cursor-mode 0)
-
 ;; org mode
 (require 'org)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(setq org-default-notes-file (concat org-directory "/inbox.org"))
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
 
 ;; magit
 (custom-set-variables
@@ -31,7 +31,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "f5b6be56c9de9fd8bdd42e0c05fecb002dedb8f48a5f00e769370e4517dde0e8" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "efcecf09905ff85a7c80025551c657299a4d18c5fcfedd3b2f2b6287e4edd659" "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
- '(package-selected-packages '(solarized-theme magit)))
+ '(org-agenda-files '("~/org/home.org"))
+ '(package-selected-packages '(helm solarized-theme magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -39,5 +40,3 @@
  ;; If there is more than one, they won't work right.
  '(show-paren-match ((t (:background "#40ff40"))))
  '(show-paren-mismatch ((((class color)) (:background "red" :foreground "white")))))
-
-
